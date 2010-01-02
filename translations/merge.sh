10e89c3522407e8bd67ae8d9050d58d4eea8ff8a
Ryan Kavanagh <ryanakca@kubuntu.org>
2010-01-01 21:16:39 -0500

    Revert "Translation added, German translation backported"
    This reverts commit 298d106b77e5dc3c34667ed2086fab70b6a12a96.
    Not required since translations were included upstream.

--- translations/merge.sh
+++
@@ -1,6 +0,0 @@
-#! /bin/bash 
-#Add your translation here to have it merged.
-
-msgmerge -U nl/bangarang.po bangarang.pot
-msgmerge -U de/bangarang.po bangarang.pot
-msgmerge -U fr/bangarang.po bangarang.pot
